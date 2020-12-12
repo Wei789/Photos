77e6bfbe8171943b77af5bcd58b640f4aad77450
鄭惟臣 <weichen.cheng@thinkpower-info.com>
2020-12-12 12:47:41 +0800

    init

---
+++ Photos/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by 鄭惟臣 on 2020/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
